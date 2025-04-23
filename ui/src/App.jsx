d447e594553d5dd68e39254e8ce4c67fe22d0043
CharcoalZz <chamodsilva@gmail.com>
2025-04-23 19:15:27 +0800

    Uploading Wednesday class

---
+++ ui/src/App.jsx
@@ -0,0 +1,25 @@
+import { Container } from "@mui/material";
+import HomePage from "./containers/HomePage";
+import "./App.css";
+
+function App()
+{
+  // VARIABLES/STATE
+
+  // FUNCTIONS/SIDE EFFECT
+
+  // RETURN OF OUR VISUAL STUFF
+  return (
+    <Container
+      sx=
+      {{
+        display: "flex",
+        height: "90vh",
+      }}
+    >
+      LANDING PAGE
+    </Container>
+  );
+}
+
+export default App;
